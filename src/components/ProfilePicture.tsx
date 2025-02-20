aa87447bc41dcc46175f673fcec15a10c6dd36d1
Alex Mejicanos <alexmejicanos@Alexs-MacBook-Pro.local>
2025-02-20 15:41:26 -0600

    Add support of pnpm to the pipeline

--- src/components/ProfilePicture.tsx
+++ src/components/ProfilePicture.tsx
@@ -7,7 +7,7 @@ export default function ProfilePicture() {
     <div className="w-full h-full relative">
       <div className="w-[298px] h-[298px] xl:w-[498px] xl:h-[398px] mix-blend-lighten">
         <Image
-          src="/assets/profile.jpg"
+          src="./assets/profile.jpg"
           priority
           quality={100}
           fill
