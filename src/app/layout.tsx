69ffeba34f29578cc0b7eb68ff9d5293c388001f
Alex Mejicanos <alexmejicanos@outlook.com>
2025-02-20 22:05:31 -0600

    Add Google analytics and update image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next';
+import Script from 'next/script';
 import { JetBrains_Mono } from 'next/font/google';
 import './globals.css';
 
@@ -25,6 +26,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-97YNKR4ED9"
+        ></Script>
+        <Script id="google-analytics">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-97YNKR4ED9');
+          `}
+        </Script>
+      </head>
       <body className={`${jetBrainsMono.variable}`}>
         <main className="flex flex-col xl:h-screen xl:justify-between">
           <div className="flex flex-col">
